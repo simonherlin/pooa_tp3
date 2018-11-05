da49f5737193d1248719a0182329a0785f130d4e
simonherlin <herlin.simon@gmail.com>
2018-11-05 16:24:43 +0100

    [RM] remove import useless

--- src/drawing/ui/PaintApplication.java
+++ src/drawing/ui/PaintApplication.java
@@ -1,13 +1,9 @@
 package drawing.ui;
 
-import drawing.handlers.*;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
